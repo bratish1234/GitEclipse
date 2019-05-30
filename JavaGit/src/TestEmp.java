9f144c2811a2fb8d27a4fda82874b987102607ec
bratish1234 <bratish25808.bk@gmail.com>
2019-05-30 04:31:03 -0700

    testemp

--- JavaGit/src/TestEmp.java
+++ JavaGit/src/TestEmp.java
@@ -1,6 +1,7 @@
 
 public class TestEmp {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Employee e1=new Employee();
@@ -8,6 +9,9 @@ public class TestEmp {
 		e1.setEmpName("bratish");
 		e1.setEmpSal(100000);
 
-	}
+
+
+}
+
 
 }
